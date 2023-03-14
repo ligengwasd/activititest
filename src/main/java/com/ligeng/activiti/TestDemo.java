97c5e5776edd633d2a59b1134abd837278c86bde
geng.li <geng.li@timschina.com>
2023-03-14 14:54:11 +0800

    update

--- src/main/java/com/ligeng/activiti/TestDemo.java
+++ src/main/java/com/ligeng/activiti/TestDemo.java
@@ -2,6 +2,8 @@ package com.ligeng.activiti;
 
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.repository.Deployment;
 import org.junit.Test;
 
 public class TestDemo {
@@ -10,5 +12,21 @@ public class TestDemo {
         //使用classpath下的activiti.cfg.xml中的配置创建processEngine
         ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
         System.out.println(processEngine);
+    }
+    @Test
+    public void testDeployment(){
+//        1、创建ProcessEngine
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+//        2、得到RepositoryService实例
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+//        3、使用RepositoryService进行部署
+        Deployment deployment = repositoryService.createDeployment()
+                .addClasspathResource("bpmn/evection.bpmn") // 添加bpmn资源
+                // .addClasspathResource("bpmn/evection.png")  // 添加png资源
+                .name("出差申请流程")
+                .deploy();
+//        4、输出部署信息
+        System.out.println("流程部署id：" + deployment.getId());
+        System.out.println("流程部署名称：" + deployment.getName());
     }
 }
