0fea1467f3f78ad5f3bcd09ce9abf843fe878b7c
geng.li <geng.li@timschina.com>
2023-03-14 15:02:21 +0800

    update

--- src/main/java/com/ligeng/activiti/TestDemo.java
+++ src/main/java/com/ligeng/activiti/TestDemo.java
@@ -1,10 +1,16 @@
 package com.ligeng.activiti;
 
-import org.activiti.engine.ProcessEngine;
-import org.activiti.engine.ProcessEngines;
-import org.activiti.engine.RepositoryService;
+import org.activiti.engine.*;
+import org.activiti.engine.history.HistoricActivityInstance;
+import org.activiti.engine.history.HistoricActivityInstanceQuery;
 import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.repository.ProcessDefinitionQuery;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
 import org.junit.Test;
+
+import java.util.List;
 
 public class TestDemo {
     @Test
@@ -28,5 +34,136 @@ public class TestDemo {
 //        4、输出部署信息
         System.out.println("流程部署id：" + deployment.getId());
         System.out.println("流程部署名称：" + deployment.getName());
+    }
+    /**
+     * 启动流程实例
+     */
+    @Test
+    public void testStartProcess(){
+//        1、创建ProcessEngine
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+//        2、获取RunTimeService
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+//        3、根据流程定义Id启动流程
+        ProcessInstance processInstance = runtimeService
+                .startProcessInstanceByKey("myEvection");
+//        输出内容
+        System.out.println("流程定义id：" + processInstance.getProcessDefinitionId());
+        System.out.println("流程实例id：" + processInstance.getId());
+        System.out.println("当前活动Id：" + processInstance.getActivityId());
+    }
+    /**
+     * 查询当前个人待执行的任务
+     */
+    @Test
+    public void testFindPersonalTaskList() {
+//        任务负责人
+        String assignee = "zhangsan";
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+//        创建TaskService
+        TaskService taskService = processEngine.getTaskService();
+//        根据流程key 和 任务负责人 查询任务
+        List<Task> list = taskService.createTaskQuery()
+                .processDefinitionKey("myEvection") //流程Key
+                .taskAssignee(assignee)//只查询该任务负责人的任务
+                .list();
+
+        for (Task task : list) {
+
+            System.out.println("流程实例id：" + task.getProcessInstanceId());
+            System.out.println("任务id：" + task.getId());
+            System.out.println("任务负责人：" + task.getAssignee());
+            System.out.println("任务名称：" + task.getName());
+
+        }
+    }
+    // 完成任务
+    @Test
+    public void completTask(){
+//        获取引擎
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+//        获取taskService
+        TaskService taskService = processEngine.getTaskService();
+
+//        根据流程key 和 任务的负责人 查询任务
+//        返回一个任务对象
+        Task task = taskService.createTaskQuery()
+                .processDefinitionKey("myEvection") //流程Key
+                .taskAssignee("zhangsan")  //要查询的负责人
+                .singleResult();
+
+//        完成任务,参数：任务id
+        taskService.complete(task.getId());
+    }
+    /**
+     * 查询流程定义
+     */
+    @Test
+    public void queryProcessDefinition(){
+        //        获取引擎
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+//        repositoryService
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+//        得到ProcessDefinitionQuery 对象
+        ProcessDefinitionQuery processDefinitionQuery = repositoryService.createProcessDefinitionQuery();
+//          查询出当前所有的流程定义
+//          条件：processDefinitionKey =evection
+//          orderByProcessDefinitionVersion 按照版本排序
+//        desc倒叙
+//        list 返回集合
+        List<ProcessDefinition> definitionList = processDefinitionQuery.processDefinitionKey("myEvection")
+                .orderByProcessDefinitionVersion()
+                .desc()
+                .list();
+//      输出流程定义信息
+        for (ProcessDefinition processDefinition : definitionList) {
+            System.out.println("流程定义 id="+processDefinition.getId());
+            System.out.println("流程定义 name="+processDefinition.getName());
+            System.out.println("流程定义 key="+processDefinition.getKey());
+            System.out.println("流程定义 Version="+processDefinition.getVersion());
+            System.out.println("流程部署ID ="+processDefinition.getDeploymentId());
+        }
+
+    }
+    public void deleteDeployment() {
+        // 流程部署id
+        String deploymentId = "1";
+
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        // 通过流程引擎获取repositoryService
+        RepositoryService repositoryService = processEngine
+                .getRepositoryService();
+        //删除流程定义，如果该流程定义已有流程实例启动则删除时出错
+        repositoryService.deleteDeployment(deploymentId);
+        //设置true 级联删除流程定义，即使该流程有流程实例启动也可以删除，设置为false非级别删除方式，如果流程
+        //repositoryService.deleteDeployment(deploymentId, true);
+    }
+    /**
+     * 查看历史信息
+     */
+    @Test
+    public void findHistoryInfo(){
+//      获取引擎
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+//        获取HistoryService
+        HistoryService historyService = processEngine.getHistoryService();
+//        获取 actinst表的查询对象
+        HistoricActivityInstanceQuery instanceQuery = historyService.createHistoricActivityInstanceQuery();
+//        查询 actinst表，条件：根据 InstanceId 查询
+//        instanceQuery.processInstanceId("2501");
+//        查询 actinst表，条件：根据 DefinitionId 查询
+        instanceQuery.processDefinitionId("myEvection:1:4");
+//        增加排序操作,orderByHistoricActivityInstanceStartTime 根据开始时间排序 asc 升序
+        instanceQuery.orderByHistoricActivityInstanceStartTime().asc();
+//        查询所有内容
+        List<HistoricActivityInstance> activityInstanceList = instanceQuery.list();
+//        输出
+        for (HistoricActivityInstance hi : activityInstanceList) {
+            System.out.println(hi.getActivityId());
+            System.out.println(hi.getActivityName());
+            System.out.println(hi.getProcessDefinitionId());
+            System.out.println(hi.getProcessInstanceId());
+            System.out.println("<==========================>");
+        }
     }
 }
